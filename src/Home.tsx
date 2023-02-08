53583ca91e11f681ec3a015253a493c088ade45c
ZenRepublic <hq@zenrepublic.space>
2023-02-08 19:21:30 +0200

    test

--- src/Home.tsx
+++ src/Home.tsx
@@ -448,7 +448,7 @@ const Home = (props: HomeProps) => {
       // debugger;
       candyMachineV3
         .mint(quantityString, {
-          groupLabel: "paid",
+          groupLabel: guardLabel,
           nftGuards,
         })
         .then((items) => {
